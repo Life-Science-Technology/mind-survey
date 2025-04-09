d958f6f3c18d4293eef4a70a45d38145109a516c
gunho-noh <gunho.noh@lstgrp.com>
2025-04-09 15:30:30 +0900

    BrowserRouter를 HashRouter로 변경하여 GitHub Pages에서 라우팅 문제 해결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import IntroPage from './components/IntroPage';
 import InfoForm from './components/InfoForm';
